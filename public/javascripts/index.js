8ae24af629ae8e26b36581449d2660616d2538b9
DeveloperAbhirupa <abhirupa.mitra2017@vitstudent.ac.in>
2018-10-21 13:58:19 +0530

    Code pushed

---
+++ public/javascripts/index.js
@@ -0,0 +1,85 @@
+firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+        alert("You have been logged in");
+    } else {
+        console.log("Not logged in");
+    }
+});
+
+function login(){
+    mail=$("#email").val();
+    psw=$("#pwd").val();
+    console.log(mail,psw,"is the pass");
+
+    firebase.auth().signInWithEmailAndPassword(mail, psw).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        alert("Error",errorCode,":",errorMessage);
+    });
+    location.href = "chatRoom.html";
+    return false;
+}
+
+function signUp(){
+    name=$("#nameS").val();
+    mailS=$("#mailS").val();
+    pwdS=$("#pwdS").val();
+    pwdS2=$("#pwdS2").val();
+    firebase.auth().createUserWithEmailAndPassword(mailS, pwdS2).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        alert("Error",errorCode,":",errorMessage);
+        // ...
+    });
+    window.location = "index3.html";
+    return false;
+}
+
+$("#GoogleIcon").click(
+function googleLogin(){
+
+    // console.log(href("index3.html"));
+
+    var provider = new firebase.auth.GoogleAuthProvider();
+    console.log(provider);
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        console.log("Success");
+        var token = result.credential.accessToken;
+        // The signed-in user info.
+        var user = result.user;
+        // ...
+    }).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        console.log("Error",errorCode,"msg:",errorMessage,"mail:",email);
+        // ...
+    });
+
+    //Getting redirected result
+    firebase.auth().getRedirectResult().then(function(result) {
+        if (result.credential) {
+            // This gives you a Google Access Token. You can use it to access the Google API.
+            var token = result.credential.accessToken;
+        }
+        var user = result.user;
+    }).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        var email = error.email;
+        var credential = error.credential;// The firebase.auth.AuthCredential type that was used.
+
+    });
+
+    window.location= "index3.html";
+    console.log("Signed in with Google");
+    return false;
+});
---
+++ public/javascripts/main.js
@@ -0,0 +1,90 @@
+
+initFirebase = function() {
+    // Shortcuts to Firebase SDK features.
+    this.auth = firebase.auth();
+    this.database = firebase.database();
+    this.storage = firebase.storage();
+    this.messaging = firebase.messaging();
+
+    // Initiates Firebase auth and listen to auth state changes.
+    this.auth.onAuthStateChanged(this.authStateObserver.bind(this));
+};
+
+authStateObserver = function(user) {
+        this.saveMessagingDeviceToken();
+        console.log("Calling saveMesssagingDeviceToken");
+};
+
+
+
+
+function loadMessages(){
+    console.log("loadMessages running");
+    var userDataRef = firebase.database().ref("messages").orderByKey();
+    userDataRef.once("value").then(function(snapshot) {
+        snapshot.forEach(function(childSnapshot) {
+            var key = childSnapshot.key;
+            var childData = childSnapshot.val();
+
+            var messageUser = childSnapshot.val().msg;
+            var user = childSnapshot.val().name;
+
+            $("#name").append(user+'</br>');
+            $("#text").append(messageUser+'</br>');
+
+        });
+    });
+
+}
+function saveMessage(messageText)
+{
+    console.log("saveMessage running");
+    console.log(firebase.auth().currentUser.displayName);
+    return firebase.database().ref('/messages/').push({
+        name:firebase.auth().currentUser.displayName,
+        msg:messageText
+
+    }).catch(function(error){
+        console.error ("Cannot write new messages to database");
+    });
+}
+
+loadMessages();
+ function readMessage(){
+         console.log("Submit clicked");
+         var messageText=$('#msg').val();
+         saveMessage(messageText);
+         $("#name").html("");
+         $("#text").html("");
+         loadMessages();
+ }
+//Save FCM device token for notification
+ function saveMessagingDeviceToken(){
+     firebase.messaging().getToken().then(function(currentToken) {
+         if (currentToken) {
+             console.log('Got FCM device token:', currentToken);
+             // Saving the Device Token to the datastore.
+             firebase.database().ref('/fcmTokens').child(currentToken)
+                 .set(firebase.auth().currentUser.uid);
+         } else {
+             // Need to request permissions to show notifications.
+             requestNotificationsPermissions();
+         }
+     }).catch(function(error){
+         console.error('Unable to get messaging token.', error);
+     });
+ };
+
+ //Request action for Notification Permission
+function requestNotificationsPermissions() {
+    console.log('Requesting notifications permission...');
+    firebase.messaging().requestPermission().then(function() {
+        // Notification permission granted.
+        saveMessagingDeviceToken();
+    }).catch(function(error) {
+        console.error('Unable to get permission to notify.', error);
+    });
+};
+
+
+initFirebase();
